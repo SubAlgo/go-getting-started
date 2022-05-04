733621c7f2250474dd4c6f2fbe64dd2df3cb4d4b
Kornchaval <kornchaval@gmail.com>
2022-05-04 15:54:37 +0700

    start test line

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+/*
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/heroku/x v0.0.33
+)
+
+
+ channelSecret: "af42dc438bbcf308b5b0d274b4e1846e"
+  channelAccessToken: "CX7kjGwq6ASjy3wd2SRihDD4XhlEzVKbTQ07JIUqGhNhXHuQwJ1L9NdP80uvSpqFz7qpmsdSQO0r9HmvEITCUGoy4j/zJWxwx09+5P8Mklzbo1H2FBnrrPXYx3iFhl+iZU74LMu0q8HEpQCj/vk1DgdB04t89/1O/w1cDnyilFU="
+  yourLineID: "Ue56654f7c4a75297a0ecd19695fef261"
+*/
 func main() {
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -17,13 +29,6 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK",
